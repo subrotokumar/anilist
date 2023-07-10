8b53276441f0ec42bca04739de81ac84e6682392
Subroto Kumar <subrotokumar@outlook.in>
2023-07-11 01:40:00 +0530

    v1.0.0
    Initial Commit

---
+++ lib/fav_anime_query.dart
@@ -0,0 +1,3 @@
+export 'package:anilist/graphql/__generated__/FavAnimeQuery.ast.gql.dart';
+export 'package:anilist/graphql/__generated__/FavAnimeQuery.data.gql.dart';
+export 'package:anilist/graphql/__generated__/FavAnimeQuery.var.gql.dart';
